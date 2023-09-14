377d50fc9200ab7862e5a14a1ac2fe3a3d669fb7
Natalia-Nowak <nowak.natalia.080@gmail.com>
2023-09-14 12:23:03 +0200

    added styles

--- src/cat-api.js
+++ src/cat-api.js
@@ -42,29 +42,7 @@ export const fetchCatByBreed = breedId => {
   catInfo.style.visibility = 'hidden';
   loader.style.visibility = 'visible';
 
-  fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`, options)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      const name = document.createElement('h2');
-      const description = document.createElement('p');
-      const temperament = document.createElement('p');
-      name.innerHTML = data.name;
-      description.innerHTML = data.description;
-      temperament.innerHTML = data.temperament;
-
-      catInfo.append(name);
-      catInfo.append(description);
-      catInfo.append(temperament);
-
-      catInfo.style.visibility = 'visible';
-      loader.style.visibility = 'hidden';
-      error.style.visibility = 'hidden';
-    });
-
-  return fetch(url, options)
+  fetch(url, options)
     .then(response => {
       return response.json();
     })
@@ -75,10 +53,38 @@ export const fetchCatByBreed = breedId => {
       console.log(imgUrl);
       img.src = imgUrl;
       catInfo.append(img);
-      img.maxWidth = 500;
-      img.height = 400;
+      img.maxWidth = 400;
+      img.height = 200;
     })
     .catch(e => {
       error.style.visibility = 'visible';
+    });
+
+  return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`, options)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      const name = document.createElement('h2');
+      const description = document.createElement('p');
+      const temperament = document.createElement('p');
+      const descriptionBlock = document.createElement('div');
+
+      name.innerHTML = data.name;
+      description.innerHTML = data.description;
+      temperament.innerHTML = data.temperament;
+
+      descriptionBlock.appendChild(name);
+      descriptionBlock.appendChild(description);
+      descriptionBlock.appendChild(temperament);
+      catInfo.append(descriptionBlock);
+
+      catInfo.style.visibility = 'visible';
+      loader.style.visibility = 'hidden';
+      error.style.visibility = 'hidden';
+      catInfo.style.display = 'flex';
+      catInfo.style.gap = '20px';
+      descriptionBlock.style.width = '500px';
     });
 };
